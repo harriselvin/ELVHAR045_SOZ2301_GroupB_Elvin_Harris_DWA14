c54d14253fa69e6f159ec1069c46336c63108551
Elvin Harris <harriselvin6@gmail.com>
2023-07-16 13:42:45 +0200

    DWA14

---
+++ DWA14/JavaScript/Script.js
@@ -0,0 +1,29 @@
+import "../Components/tally-count.js";
+
+const machine = {
+  active: "locked",
+  /**
+   * @type {Record<string, State>}
+   */
+  states: {
+    locked: {
+      type: "locked",
+      pushTurnstile: () => {
+        throw new Error("It is locked");
+      },
+      insertCoin: () => (machine.active = "unlocked"),
+    },
+    unlocked: {
+      type: "unlocked",
+      pushTurnstile: () => (machine.active = "locked"),
+      insertCoin: () => {
+        throw new Error("already unlocked");
+      },
+    },
+  },
+};
+
+const push = document.querySelector("[data-push]");
+const coin = document.querySelector("[data-coin]");
+
+push?.addEventListener("click", () => {});
